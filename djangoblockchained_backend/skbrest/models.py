13b40a6f9a687a627c4c153e4d6701395d318016
rene-karrass <rene.karrass@campus.tu-berlin.de>
2018-05-27 18:08:34 +0200

    basic database

---
+++ djangoblockchained_backend/skbrest/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
+
+# Create your models here.
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+class Teacher(models.Model):
+    student = models.OneToOneField(Student, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.student.user.username
+
+#class Admin()
+
+#class Moderator()
+
+class Course(models.Model):
+    name = models.TextField(unique=True)
+    students = models.ManyToManyField(Student)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    deadline = models.DateTimeField()
+    student_count = models.IntegerField(validators=[MinValueValidator(0)])
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+    
