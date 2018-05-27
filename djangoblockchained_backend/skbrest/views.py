50ecc47fb56fd277b683b71d66dd071fbbaedc28
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2018-05-27 19:37:27 +0200

    basic auth

--- djangoblockchained_backend/skbrest/views.py
+++ djangoblockchained_backend/skbrest/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
+
+def authenticate_user(request):
+    if request.method == "POST":
+        
+        user = request.POST.get("username")
+        passw = request.POST.get("password")
+        isValidUser = authenticate(username = user, password = passw)
+
+        if isValidUser:
+
+            return JsonResponse({"result": True})
+        else:
+
+            return JsonResponse({"result": False})
