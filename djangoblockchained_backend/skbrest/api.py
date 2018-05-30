ff3a134888a65f8168d072dfc22a8503a476a614
rene-karrass <rene.karrass@campus.tu-berlin.de>
2018-05-30 19:19:04 +0200

    add register

---
+++ djangoblockchained_backend/skbrest/api.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User # If used custom user model
+
+from .serializers import UserSerializer
+
+
+class CreateUserView(CreateAPIView):
+
+    model = User
+    permission_classes = [
+        permissions.AllowAny # Or anon users can't register
+    ]
+    serializer_class = UserSerializer
--- djangoblockchained_backend/skbrest/urls.py
+++ djangoblockchained_backend/skbrest/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include 
 from .views import authenticate_user
+from .api import CreateUserView
 
 urlpatterns = [
-    path('auth', authenticate_user)
+    path('auth', authenticate_user),
+    path('register', CreateUserView.as_view())
 ]
