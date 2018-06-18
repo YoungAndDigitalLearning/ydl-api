c7635e91f0da7b71708f91491a614122c4bd3fc8
B4rtware <34386047+B4rtware@users.noreply.github.com>
2018-06-18 23:05:33 +0200

    modified tests

--- djangoblockchained_backend/skbrest/tests.py
+++ djangoblockchained_backend/skbrest/tests.py
@@ -30,44 +30,57 @@ class UserTests(APITestCase):
 
 class CourseTests(APITestCase):
     def setUp(self):
-        self.teacher = Teacher(
+        # create one dummy teacher
+        self.teacher = Teacher.objects.create(
             user = User.objects.create_user(
                 username = "Croozer",
                 email = "Croozer@Croozer.com"
             )
         )
-        self.teacher.save()
-
-        self.courses = Course(
-            name = "Croozers Logic Course",
-            teacher_id = self.teacher.user.id,
-            description = "Keep Kroozin!",
-            deadline = datetime.datetime.now(),
-            student_count = 5,
-            price = 0
-        )
-        self.courses.save()
-
-        self.student = Student(
+        
+        # create one dummy student
+        self.student = Student.objects.create(
             user = User.objects.create_user(
                 username = "Karsten",
                 email = "Karsten@Karsten.com"
             )
         )
-        self.student.courses.add(self.courses)
-        self.student.save()
-        
 
-        # self.client = CoreAPIClient()
-        self.token = createToken(self.student.user)
-        print("Session",self.client.session.kwargs)
-        #self.client.session.headers.update({'Authorization': self.token})
+        # create courses
+        self.courses = [
+            Course.objects.create(
+                name = "Croozers Logic Course",
+                teacher_id = self.teacher.user.id,
+                description = "Keep Kroozin!",
+                deadline = datetime.datetime.now(),
+                student_count = 5,
+                price = 0
+            ),
+            Course.objects.create(
+                name = "Mathematik",
+                teacher_id = self.teacher.user.id,
+                description = "Mein Mathe",
+                deadline = datetime.datetime.now(),
+                student_count = 5,
+                price = 20
+            )
+        ]
+
+        # assign student to course
+        self.student.courses.add(self.courses[0])
+        
+        # create tokens for each user
+        self.student_token = createToken(self.student.user)
+        self.teacher_token = createToken(self.teacher.user)
+
+        self.student_client = CoreAPIClient()
+        self.student_client.session.headers.update({"Authentication": "JWT " + self.student_token})
 
     def test_get_student_courses(self):
         url = reverse("course-list")
-        response = self.client.get(url)
+        response = self.student_client.get(url)
         
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, url)
         print(response.data)
         self.assertEqual(response.data, {"id": 5})
         
