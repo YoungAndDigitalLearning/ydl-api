6ffa05e965db25ab9d622615d96638bd49c6572e
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2018-06-22 16:17:27 +0200

    add Email verified site

--- ydl_api/api/views.py
+++ ydl_api/api/views.py
@@ -31,21 +31,21 @@ jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
 
 def render_email(request):
     context = {
-        'user': 'Croozer',
-        'link': '#',
-        # change plural!
-        'expires_in': '1:00:00',
-        'expires_time': ' hours',
-        'logo_img_link': "https://ydlearning.com/.media/logo/YDL-Logo-220px.png",
-        'email_sendto': 'Croozer@ydlearning.com',
-        'ydl_email': "admin@ydlearning.com",
-        'ydl_url': "www.ydlearning.com",
-        'ydl_url_github': "https://github.com/YoungAndDigitalLearning",
-        'ydl_url_impr': "www.ydlearning.com/impressum.html",
-        'ydl_url_prpol': "www.ydlearning.com/privacypolicy",
+            'user': 'Croozer',
+            'link': 'https://api.ydlearning.com/activate/',
+            'expires_in': '1',
+            'expires_time': ' hours',  # change plural!
+            # 'logo_img_link':"",
+            'email_sendto': 'Croozer@',
+            'ydl_context': "Context Text",
+            'ydl_email': "admin@ydlearning.com",
+            'ydl_url': "https://www.ydlearning.com",
+            'ydl_url_github': "https://github.com/YoungAndDigitalLearning",
+            'ydl_url_impr': "https://www.ydlearning.com/sites/impressum.html",
     }
 
-    return render(request, "api/verification_email.html", context)
+    # return render(request, "api/verification_email.html", context)
+    return render(request, "api/verified.html", context)
 
 
 def activate(request, uidb64, token):
@@ -81,7 +81,110 @@ def activate(request, uidb64, token):
         user.isEmailActivated = True
         user.save()
         # return redirect('home')
-        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+        return HttpResponse(''' 
+        <html>
+
+<body>
+	<!-- Wrapper/Container Table: Use a wrapper table to control the width and the background color consistently of your email. Use this approach instead of setting attributes on the body tag. -->
+	<table cellpadding="0" width="100%" cellspacing="0" border="0" id="backgroundTable" class='bgBody'>
+		<tr>
+			<td>
+				<table cellpadding="0" width="620" class="container" align="center" cellspacing="0" border="0">
+					<tr>
+						<td>
+							<!-- Tables are the most common way to format your email consistently. Set your table widths inside cells and in most cases reset cellpadding, cellspacing, and border to zero. Use nested tables as a way to space effectively in your message. -->
+
+
+							<table cellpadding="0" cellspacing="0" border="0" align="center" width="600" class="container">
+								<tr>
+									<td class='movableContentContainer bgItem'>
+
+										<div class='movableContent'>
+											<table cellpadding="0" cellspacing="0" border="0" align="center" width="600" class="container">
+												<tr height="40">
+													<td width="200">&nbsp;</td>
+													<td width="200">&nbsp;</td>
+													<td width="200">&nbsp;</td>
+												</tr>
+
+												<tr height="25">
+													<td width="200">&nbsp;</td>
+													<td width="200">&nbsp;</td>
+													<td width="200">&nbsp;</td>
+												</tr>
+
+											</table>
+										</div>
+
+										<div class='movableContent'>
+											<table cellpadding="0" cellspacing="0" border="0" align="center" width="600" class="container">
+												<tr>
+													<td width="100%" colspan="3" align="center" style="padding-bottom:0px;padding-top:0px;">
+														<div class="contentEditableContainer contentTextEditable">
+															<div class="contentEditable" align='center'>
+																<h1>Email verified</h1>
+															</div>
+														</div>
+													</td>
+												</tr>
+												<tr>
+													<td width="100">&nbsp;</td>
+													<td width="400" align="center">
+														<div class="contentEditableContainer contentTextEditable">
+															<div class="contentEditable" align='left'>
+																<br>
+																<p>
+																	Thank you for verification.
+																	<br>
+																	<br/> You can now use all features of
+																	<i style="color:#0689b3;">
+																		<b>Y&D Learning</b>
+																	</i>.
+																	<br/>
+																	<br/> Go to mainpage:
+																</p>
+															</div>
+														</div>
+													</td>
+													<td width="100">&nbsp;</td>
+												</tr>
+											</table>
+											<table cellpadding="0" cellspacing="0" border="0" align="center" width="600" class="container">
+												<tr>
+													<td width="200">&nbsp;</td>
+													<td width="200" align="center" style="padding-top:20px;">
+														<table cellpadding="0" cellspacing="0" border="0" align="center" width="200" height="50">
+															<tr>
+																<td bgcolor="#0689b3" align="center" style="border-radius:4px;" width="200" height="50">
+																	<div class="contentEditableContainer contentTextEditable">
+																		<div class="contentEditable" align='center'>
+																			<a target='_blank' href="https://ydlearning.com/" class='link2'>Homepage</a>
+																		</div>
+																	</div>
+																</td>
+															</tr>
+														</table>
+													</td>
+													<td width="200">&nbsp;</td>
+												</tr>
+											</table>
+										</div>
+
+
+									</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+				</table>
+
+			</td>
+		</tr>
+	</table>
+</body>
+
+</html>
+        ''')
     else:
         return HttpResponse('Activation link is invalid!')
 
