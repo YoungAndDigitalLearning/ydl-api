0c709160bc24346288bf66c9734521ca163d7b80
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2018-07-09 18:25:40 +0200

    fix score, max_score isnt required

--- ydl_api/quiz/serializers.py
+++ ydl_api/quiz/serializers.py
@@ -17,8 +17,8 @@ class TaskSerializer(serializers.ModelSerializer):
 
 class TestSerializer(serializers.ModelSerializer):
     tasks = serializers.SerializerMethodField()
-    score = serializers.IntegerField(source='get_score')
-    max_score = serializers.IntegerField(source='get_max_score')
+    score = serializers.IntegerField(source='get_score', read_only = True)
+    max_score = serializers.IntegerField(source='get_max_score', read_only = True)
 
     def get_tasks(self, test):
         return [TaskSerializer(task).data for task in m.Task.objects.filter(test=test)]
