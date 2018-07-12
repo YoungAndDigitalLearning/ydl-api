edf3f2c7ad141e81092301eb08a67ca4eeae3e21
B4rtware <34386047+B4rtware@users.noreply.github.com>
2018-07-13 00:39:31 +0200

    removed resource return in courses

--- ydl_api/api/serializers.py
+++ ydl_api/api/serializers.py
@@ -119,11 +119,7 @@ class CourseSerializer(serializers.ModelSerializer):
     posts = serializers.SerializerMethodField()
     events = serializers.SerializerMethodField()
     weeks = serializers.SerializerMethodField()
-    resources = serializers.SerializerMethodField()
     tests = serializers.PrimaryKeyRelatedField(many=True, read_only=True, source="test_set")
-
-    def get_resources(self, course):
-        return [ResourceSerializer(resource).data for resource in course.resources.all()]
 
     def get_posts(self, course):
         return [PostSerializer(post).data["id"] for post in Post.objects.filter(course=course, childs__isnull=True)]
